6543074585b2ff129e523eba48306203c5845f2c
David Welker <david.v.welker@gmail.com>
2016-06-16 23:50:13 -0700

    Modification to docs.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.0',
+    version='3.0.0',
 
     description='NDEx Python includes a client and a data model.',
     long_description=long_description,
