ad93075ca400db698aea86f550a320d47bd6ef7e
vrynkov <vrynkov@yahoo.com>
2017-01-19 16:55:30 -0800

    Adding finished TestProvenanceAndProfile.py and TestSystemProperties.py unit tests; TestProperties.py unit test is the work in progress; A549-SL-network.cx is network used by unit tests; testing.py is a testing script (not a unit test, but a script that tests many APIs).

---
+++ ndex/test/TestProperties.py
@@ -0,0 +1,61 @@
+import unittest
+
+import ndex.client as nc
+import time
+
+
+ndex_host = "http://dev.ndexbio.org"
+ndex_network_resource = "/v2/network/"
+username_1 = "ttt"
+password_1 = "ttt"
+
+example_network_1 = './A549-SL-network.cx'
+
+# Python Client APIs tested:
+#
+#   set_network_properties
+#
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_network_system_properties(self):
+        ndex = nc.Ndex(host=ndex_host, username=username_1, password=password_1)
+
+        with open(example_network_1, 'r') as file_handler:
+            network_in_cx = file_handler.read()
+
+        # test save_cx_stream_as_new_network
+        test_network_1_uri = ndex.save_cx_stream_as_new_network(network_in_cx)
+        self.assertTrue(test_network_1_uri.startswith(ndex_host + ndex_network_resource))
+
+        network_UUID = str(test_network_1_uri.split("/")[-1])
+
+        # get network summary
+        network_summary = ndex.get_network_summary(network_UUID)
+
+        if "subnetworkIds" in network_summary:
+            number_of_subnetworks = len(network_summary["subnetworkIds"])
+            self.assertTrue(number_of_subnetworks == 1, "Expected 1 subnetwork in network summary, but there are " \
+                            + str(number_of_subnetworks))
+
+        subnetwork_id = network_summary["subnetworkIds"][0]
+
+
+
+
+        # test set_network_system_properties
+        time.sleep(10)
+
+
+
+        # test delete_network
+        del_network_return = ndex.delete_network(network_UUID)
+        self.assertTrue(del_network_return == '')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
